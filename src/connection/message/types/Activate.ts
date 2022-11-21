3376035477199ab298453f0ef47639cf5a5682e6
lukeanders70 <luke.larson@berkeley.edu>
2022-11-21 08:52:15 -0800

    minor fixes

--- src/connection/message/types/Activate.ts
+++ src/connection/message/types/Activate.ts
@@ -15,5 +15,16 @@ export function CreateActivateMessage(connectionId : string, clientName: string)
 }
 
 export function ValidateActivateMessage(message: ActivateMessage) {
-    return message.connectionId != null && message.connectionId != "" && message.clientName != null && message.clientName != ""
+    return message.connectionId != undefined && message.connectionId != "" && message.clientName != undefined && message.clientName != ""
+}
+
+
+export interface ActivateResponseMessage extends BaseMessage {
+    connectionId: string
+    clientName: string
+    success: boolean
+}
+
+export function ValidateActivateResponseMessage(message: ActivateResponseMessage) : boolean {
+    return message.connectionId !== undefined && message.clientName !== undefined && message.success !== undefined
 }
